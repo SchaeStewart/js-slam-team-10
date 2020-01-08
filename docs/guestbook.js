80d435ea810fad255b2693d861a9ff76f33f2c3d
Cameron Hetzler <cjhetz@gmail.com>
2020-01-08 18:24:47 -0500

    altering api endpoint url

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://https://9b923093.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://08be99c5.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
